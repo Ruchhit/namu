0afa6921aad70da9251de8839b950340c431b15b
Ruchhit <ruchitgule2219@gmail.com>
2023-11-08 16:38:00 +0530

    added : Shimmer.ui

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,63 +2,10 @@ import React, { useEffect, useState } from "react";
 import objData from "../utils/mockData";
 import CardContainer from "./CardContainer";
 import { SWIGGY_API } from "../utils/constants";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setlistOfRestaurants] = useState([{
-    "info": 
-    {
-      "id": "61823",
-      "name": "Smokin' Joe's",
-      "cloudinaryImageId": "azevobwtle9dlkw1xfxm",
-      "cuisines": ["Pizzas"],
-      "avgRating": 3.8,
-      "avgRatingString": "3.8",
-    }
-  },
-  {
-    "info": 
-    {
-      "id": "117397",
-      "name": "La Pino'z Pizza",
-      "cloudinaryImageId": "pft1dcdx8udfepmcnuty",
-      "cuisines": ["Pizzas", "Pastas", "Italian", "Desserts", "Beverages"],
-      "avgRating": 4.2,
-      "avgRatingString": "4.2",
-    }
-  },
-  {
-    "info": 
-    {
-      "id": "117398",
-      "name": "Kulhad pizza",
-      "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-      "cuisines": ["Pizzas", "Desserts", "Beverages"],
-      "avgRating": 4.9,
-      "avgRatingString": "4.2",
-    }
-  },
-  {
-    "info": 
-    {
-      "id": "117399",
-      "name": "Ryan's Kitchen",
-      "cloudinaryImageId": "s6fhwzl0tss0vgrqvcid",
-      "cuisines": ["Desserts", "Beverages"],
-      "avgRating": 1.2,
-      "avgRatingString": "4.2",
-    }
-  },
-  {
-    "info": 
-    {
-      "id": "117797",
-      "name": "Sardarjii",
-      "cloudinaryImageId": "zrusx2rcjtsxpksm4lne",
-      "cuisines": [  "Pastas", "Italian", "Desserts", "Beverages"],
-      "avgRating": 2.2,
-      "avgRatingString": "4.2",
-    }
-  }]);
+  const [listOfRestaurants, setlistOfRestaurants] = useState([]);
 
   useEffect(()=>{
     fetchData();
@@ -69,7 +16,11 @@ const Body = () => {
    const json = await data.json();
    setlistOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
-
+ if(listOfRestaurants.length === 0)
+ {
+    // return <h1> LOADING .....</h1>
+    return <Shimmer/>
+ }
   return (
     <div className="body">
       <div className="filter">
---
+++ src/Components/Shimmer.js
@@ -0,0 +1,23 @@
+const Shimmer =()=>{
+    return (
+        <div> 
+        <div className="filtercard"></div>
+         <div className="shimcard-container">
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+                <div className="shimcard"></div>
+        </div>
+    </div>
+    );
+}
+export default Shimmer;
