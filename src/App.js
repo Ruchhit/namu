92b142b1f79b4da61d9d386c9fdec7e059a76ced
Ruchhit <ruchitgule2219@gmail.com>
2023-11-14 13:12:27 +0530

    outlet and link component added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Header from "./Components/Header";
 import Body from "./Components/Body";
 import Footer from "./Components/Footer";
 import { Error } from "./Components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider ,Outlet } from "react-router-dom";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
+import { Children } from "react/cjs/react.production.min";
 
 // food ordering app
 
@@ -14,7 +15,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+       <Outlet/>
       <Footer />
     </div>
   );
@@ -23,17 +24,23 @@ const AppLayout = () => {
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout/>,
-    errorElement: <Error/>
-  },
-  {
-    path: "/about",
-    element: <About/>,
-  },
-  {
-    path: "/contact",
-    element: <Contact/>,
-  },
+    element: <AppLayout/>, // parent
+    errorElement: <Error/>,
+    children : [
+      {
+        path: "/",
+        element: <Body/>,
+      },
+      {
+        path: "/about",
+        element: <About/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>,
+      },
+    ],
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = ()=>{
     const[btnReact,setbtnReact]=useState('login');
@@ -10,9 +11,10 @@ const Header = ()=>{
             </div>
             <div className='nav-items'>
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Cart</li>
+                    <li><Link to={'/'}>Home</Link></li>
+                    <li><Link to={'/about'}> About us</Link></li>
+                    <li><Link to={'/contact'}> Contact us</Link></li>
+                    
                     <button className="btn" onClick={()=>
                      btnReact === 'login' ? setbtnReact('logout') : setbtnReact('login')
                     }> {btnReact} </button>
