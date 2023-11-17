71d16ebe768d095af86819afa70fce5079f65b1d
Ruchhit <ruchitgule2219@gmail.com>
2023-11-17 21:59:26 +0530

    done with the coustom hook

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,34 +1,17 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Shimmer from "./Shimmer";
-import { MENU_API } from "../utils/constants";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  // const [resInfo2, setResInfo2] = useState(null);
+
   const { resId } = useParams();
- const resInfo2 = useRestaurantMenu(resId);
+  const resInfo = useRestaurantMenu(resId);
 
-  // useEffect(() => {
-  //   fetchMenuItems();
-  // }, []);
-
-  // const fetchMenuItems = async () => {
-  //   try {
-  //     const Response = await fetch(MENU_API+resId);
-  //     const json1 = await Response.json();
-  //     setResInfo2(json1.data.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card.itemCards || []);
-  //     console.log(resInfo2, "resInfo2");
-  //   } catch (error) {
-  //     console.error("Error fetching menu items:", error);
-  //   }
-  // };
   return (
     <>
-      {resInfo2 ? (
+      {resInfo ? (
         <div className="menu">
         <h1>MENU</h1>
-          {resInfo2 && resInfo2.map((items) => {
+          {resInfo && resInfo.map((items) => {
               return (
                 <div key={items.card.info.name}>
                   <ul>
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,20 +1,28 @@
 import { useState, useEffect } from "react";
 import { MENU_API } from "./constants";
 
-const useRestaurantMenu = (resId)=>{
-    const [resInfo, setResInfo] = useState(null);
-    useEffect(() => {
-        fetchMenuItems();
-      }, []);
-    
-      const fetchMenuItems = async () => {
-        try {
-          const Response = await fetch(MENU_API+resId);
-          const json1 = await Response.json();
-          setResInfo(json1.data.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card.itemCards || []);
-          console.log(resInfo, "resInfo");
-        } catch (error) {
-          console.error("Error fetching menu items:", error);
-        }
-}};
+const useRestaurantMenu = (resId) => {
+  const [resInfo, setResInfo] = useState(null);
+
+  useEffect(() => {
+    fetchMenuItems();
+  }, [resId]);
+
+  const fetchMenuItems = async () => {
+    try {
+      const response = await fetch(MENU_API + resId);
+      const json1 = await response.json();
+      setResInfo(
+        json1.data.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+          ?.card.itemCards || []
+      );
+      console.log(resInfo, "resInfo");
+    } catch (error) {
+      console.error("Error fetching menu items:", error);
+    }
+  };
+
+  return resInfo; // Don't forget to return the state from the hook
+};
+
 export default useRestaurantMenu;
